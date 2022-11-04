91288d7dba8a35d2dec962e54f6608f07d4e999f
Parker Mortensen <parker.b.mortensen@gmail.com>
2022-11-03 19:03:34 -0700

    reverted to "routes"

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/', require('./controllers'))
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`) 
